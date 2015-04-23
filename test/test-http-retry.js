2432c70ada549867b178186743360f0f994f5594
Malte-Thorben Bruns <skenqbx@gmail.com>
2015-04-23 22:51:54 +0200

    test: add retry plugin tests

---
+++ test/test-http-retry.js
@@ -0,0 +1,55 @@
+'use strict';
+/* global suite: false, setup: false, test: false,
+    teardown: false, suiteSetup: false, suiteTeardown: false */
+var assert = require('assert');
+var common = require('./common');
+var http = require('http');
+var RAIL = require('../');
+
+
+suite('http:retry', function() {
+  var rail, server;
+  var onrequest;
+
+  var listener = function(request, response) {
+    if (typeof onrequest === 'function') {
+      onrequest(request, response);
+    }
+  };
+
+
+  suiteSetup(function(done) {
+    rail = new RAIL();
+    rail.use('retry', {
+      limit: 3,
+      interval: 20
+    });
+
+    server = http.createServer(listener);
+    server.listen(common.port, done);
+  });
+
+
+  test('call', function(done) {
+    var retries = 0;
+
+    rail.call({
+      proto: 'http',
+      port: 55555
+    }).on('error', function(err) {
+      assert(err);
+      assert.strictEqual(retries, 3);
+      assert.strictEqual(err.code, 'ECONNREFUSED');
+      done();
+    }).on('retry', function(options) {
+      assert(options);
+      assert(options.retry);
+      ++retries;
+    }).end();
+  });
+
+
+  suiteTeardown(function(done) {
+    server.close(done);
+  });
+});
