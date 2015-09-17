81b05a82fde901775da0f0e8dbceb2f4e1e40ea8
Malte-Thorben Bruns <skenqbx@gmail.com>
2015-09-17 16:36:44 +0200

    package: update to node 4.1

--- tools/cross-test.sh
+++ tools/cross-test.sh
@@ -9,17 +9,17 @@
 # $ echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
 #
 # $ nvm install -s 0.10
-# $ nvm install -s 0.12
+# $ nvm install -s 4.1
 # $ nvm install iojs
 #
 
 source ~/.nvm/nvm.sh
 
-nvm use iojs
-node_modules/.bin/_mocha ./test
-
-nvm use 0.12
-node_modules/.bin/_mocha ./test
-
 nvm use 0.10
+node_modules/.bin/_mocha ./test
+
+nvm use 4.1
+node_modules/.bin/_mocha ./test
+
+nvm use iojs
 node_modules/.bin/_mocha ./test
